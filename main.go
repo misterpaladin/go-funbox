b378a2f6ad05b5191035a2f7d799a643cf023864
Min De Sik <e.v.min@icloud.com>
2019-01-30 10:45:38 +1100

    Add second provider

--- main.go
+++ main.go
@@ -9,24 +9,43 @@ import (
 	"github.com/PuerkitoBio/goquery"
 )
 
-var first int
-var last int
-var updatedAt int64 = 0
+var providers = []func() string{
+	provider0,
+	provider1,
+}
 
 // Get get random picture url
 func Get() (url string) {
-	if time.Now().Unix()-updatedAt > 600 {
-		fmt.Println("Getting new pages")
-		updatedAt = time.Now().Unix()
-		first, last = getPicPages()
+	return providers[1]()
+}
+
+// GetFrom get from specific provider
+func GetFrom(provider int) (url string) {
+	if len(providers)-1 < provider {
+		fmt.Println("No such provider")
+		return ""
 	}
 
+	return providers[provider]()
+}
+
+func provider0() (url string) {
+	var first = 1
+	var last int
+
+	pagesDoc, _ := goquery.NewDocument("http://vse-shutochki.ru/kartinki-prikolnye")
+	pagesDoc.Find(".pagination ul li a").Each(func(i int, s *goquery.Selection) {
+		if i > 0 {
+			last, _ = strconv.Atoi(s.Text())
+		}
+	})
+
 	page := random(first, last)
-	doc, _ := goquery.NewDocument("http://vse-shutochki.ru/kartinki-prikolnye/" + strconv.Itoa(page))
+	picturesDoc, _ := goquery.NewDocument("http://vse-shutochki.ru/kartinki-prikolnye/" + strconv.Itoa(page))
 
 	pictures := make([]string, 0)
 
-	doc.Find(".post .hidden-phone").Each(func(i int, s *goquery.Selection) {
+	picturesDoc.Find(".post .hidden-phone").Each(func(i int, s *goquery.Selection) {
 		page, _ := s.Attr("src")
 		pictures = append(pictures, page)
 	})
@@ -36,17 +55,30 @@ func Get() (url string) {
 	return url
 }
 
-func getPicPages() (first int, last int) {
-	doc, _ := goquery.NewDocument("http://vse-shutochki.ru/kartinki-prikolnye")
-	doc.Find(".pagination ul li a").Each(func(i int, s *goquery.Selection) {
-		if i == 0 {
-			first, _ = strconv.Atoi(s.Text())
-		} else {
+func provider1() (url string) {
+	var first = 1
+	var last int
+
+	pagesDoc, _ := goquery.NewDocument("https://bugaga.ru/tags/прикольные+картинки/")
+	pagesDoc.Find(".navigation a").Each(func(i int, s *goquery.Selection) {
+		if i > 0 {
 			last, _ = strconv.Atoi(s.Text())
 		}
 	})
 
-	return first, last
+	page := random(first, last)
+	picturesDoc, _ := goquery.NewDocument("https://bugaga.ru/tags/прикольные+картинки/page/" + strconv.Itoa(page))
+
+	pictures := make([]string, 0)
+
+	picturesDoc.Find(".w_news .w_cntn a.highslide").Each(func(i int, s *goquery.Selection) {
+		page, _ := s.Attr("href")
+		pictures = append(pictures, page)
+	})
+
+	url = pictures[random(0, len(pictures)-1)]
+
+	return url
 }
 
 func random(min, max int) int {
